e87ff626f2f6b66e060324a3525708da93d37665
Miso Oprendek <michal@oprendek.sk>
2016-04-04 21:47:25 +0200

    UPDATE: performance tuning employing rolling sum, sumFromIncl and sumToExcl used to control subtraction/addition of values when interval moves

--- ta4j/src/main/java/eu/verdelhan/ta4j/indicators/helpers/AverageGainIndicatorSlow.java
+++ ta4j/src/main/java/eu/verdelhan/ta4j/indicators/helpers/AverageGainIndicatorSlow.java
@@ -1,18 +1,18 @@
 /**
  * The MIT License (MIT)
- *
+ * <p/>
  * Copyright (c) 2014-2015 Marc de Verdelhan & respective authors (see AUTHORS)
- *
+ * <p/>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
  * the Software without restriction, including without limitation the rights to
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- *
+ * <p/>
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- *
+ * <p/>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -30,52 +30,28 @@ import eu.verdelhan.ta4j.indicators.CachedIndicator;
  * Average gain indicator.
  * <p>
  */
-public class AverageGainIndicator extends CachedIndicator<Decimal> {
+public class AverageGainIndicatorSlow extends CachedIndicator<Decimal> {
 
     private final Indicator<Decimal> indicator;
 
     private final int timeFrame;
 
-    public AverageGainIndicator(Indicator<Decimal> indicator, int timeFrame) {
+    public AverageGainIndicatorSlow(Indicator<Decimal> indicator, int timeFrame) {
         super(indicator);
         this.indicator = indicator;
         this.timeFrame = timeFrame;
     }
 
-    private Decimal rollingSum = null;
-    private Integer sumFromIncl = null;
-    private Integer sumToExcl = null;
-
     @Override
     protected Decimal calculate(int index) {
         Decimal result = Decimal.ZERO;
-        int from = Math.max(1, index - timeFrame + 1);
-        int to = index;
-        if (rollingSum == null) {
-            sumFromIncl = from;
-            sumToExcl = from;
-            rollingSum = Decimal.ZERO;
-        }
-        for (int i = sumFromIncl; i < from; i++) {
-            rollingSum = rollingSum.minus(getIndicatorValue(i));
-            sumFromIncl = i + 1;
+        for (int i = Math.max(1, index - timeFrame + 1); i <= index; i++) {
+            if (indicator.getValue(i - 1).isLessThan(indicator.getValue(i))) {
+                result = result.plus(indicator.getValue(i).minus(indicator.getValue(i - 1)));
             }
-        for (int i = sumToExcl; i <= to; i++) {
-            rollingSum = rollingSum.plus(getIndicatorValue(i));
-            sumToExcl = i + 1;
         }
         final int realTimeFrame = Math.min(timeFrame, index + 1);
-        return rollingSum.dividedBy(Decimal.valueOf(realTimeFrame));
-    }
-
-    private Decimal getIndicatorValue(int i) {
-        final Decimal value;
-        if (indicator.getValue(i - 1).isLessThan(indicator.getValue(i))) {
-            value = indicator.getValue(i).minus(indicator.getValue(i - 1));
-        } else {
-            value = Decimal.ZERO;
-        }
-        return value;
+        return result.dividedBy(Decimal.valueOf(realTimeFrame));
     }
 
     @Override
